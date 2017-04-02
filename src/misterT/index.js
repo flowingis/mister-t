ff74504f39bd19770e5be67c3658a09c73b5d9d0
Francesco Tassi <tassi.francesco@gmail.com>
2017-04-02 23:13:44 +0200

    Minor refactoring

--- src/misterT/index.js
+++ src/misterT/index.js
@@ -8,54 +8,48 @@ const redmineIdFromSlack = require('../ideatos').bySlackName;
 
 module.exports = (controller, bot) => {
 
-  const warnAboutTimeSheet = (timeSheet, users) => {
-    _.forEach(users, (slackId, redmineId) => {
-      const day = lastBusinessDay(moment()).format('YYYY-MM-DD');
-      timeSheet.retrieveLog((err, hours) => {
-        if(hours < 8) {
-          bot.say({
-            text: `Yesterday you have logged ${hours} hours\nTeachin' fools some basic rules! `,
-            channel: slackId
-          });
-        }
-      }, redmineId, day, day);
-    })
-  };
-
-  const appear = () => {
-    controller.hears([ 'uptime', 'identify yourself', 'who are you', 'what is your name' ],
-      'direct_message,direct_mention,mention', function (bot, message) {
-
-        const uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-          ':robot_face: I am <@' + bot.identity.name +
-          '>. I have kicked asses for ' + uptime + ' now');
-
-      });
-  };
-
-  const tellUserYesterdayHours = (timeSheet) => {
-    controller.hears('what about yesterday', 'direct_message,direct_mention,mention', (bot, message) => {
-      bot.api.users.info({ user: message.user }, (error, response) => {
-        const { redmineId } = redmineIdFromSlack(`@${response.user.name}`);
+  return {
+    warnAboutTimeSheet(timeSheet, users) {
+      _.forEach(users, (slackId, redmineId) => {
         const day = lastBusinessDay(moment()).format('YYYY-MM-DD');
         timeSheet.retrieveLog((err, hours) => {
-          bot.reply(message, `Yesterday you have logged ${hours} hours\nTeachin' fools some basic rules!`)
+		  if(hours < 8) {
+            bot.say({
+              text: `Yesterday you have logged ${hours} hours\nTeachin' fools some basic rules! `,
+              channel: slackId
+            });
+          }
         }, redmineId, day, day);
       })
-    });
-  };
+    },
 
-  return {
-    warnAboutTimeSheet,
-    tellUserYesterdayHours,
-    appear
+    tellUserYesterdayHours(timeSheet) {
+      controller.hears('what about yesterday', 'direct_message,direct_mention,mention', (bot, message) => {
+        bot.api.users.info({ user: message.user }, (error, response) => {
+          const { redmineId } = redmineIdFromSlack(`@${response.user.name}`);
+          const day = lastBusinessDay(moment()).format('YYYY-MM-DD');
+          timeSheet.retrieveLog((err, hours) => {
+            bot.reply(message, `Yesterday you have logged ${hours} hours\nTeachin' fools some basic rules!`)
+          }, redmineId, day, day);
+        })
+      });
+    },
+
+    appear() {
+      controller.hears([ 'uptime', 'identify yourself', 'who are you', 'what is your name' ],
+        'direct_message,direct_mention,mention', function (bot, message) {
+
+          const uptime = formatUptime(process.uptime());
+
+          bot.reply(message,
+            ':robot_face: I am <@' + bot.identity.name +
+            '>. I have kicked asses for ' + uptime + ' now');
+        });
+    }
   }
 };
 
-
-function formatUptime(uptime) {
+function formatUptime (uptime) {
   let unit = 'second';
   if (uptime > 60) {
     uptime = uptime / 60;

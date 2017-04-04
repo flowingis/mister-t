62dbe823f5140da83d668a9fa49cd48dc66e52b3
Francesco Tassi <tassi.francesco@gmail.com>
2017-04-04 20:58:53 +0200

    Add wit.ai deps and middleware

--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ require('dotenv').config();
 module.exports = {
   redmineApiKey: process.env.REDMINE_API_KEY,
   redmineUrl: process.env.REDMINE_URL,
-  slackToken: process.env.SLACK_BOT_TOKEN
+  slackToken: process.env.SLACK_BOT_TOKEN,
+  witServerToken: process.env.WIT_SERVER_TOKEN
 };

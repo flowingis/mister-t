d3794f6b00e6cafc58b5715a5a1b68081d028917
Francesco Tassi <tassi.francesco@gmail.com>
2017-05-30 22:36:21 +0200

    Improve code readability

--- src/hook.js
+++ src/hook.js
@@ -12,13 +12,12 @@ const restService = express()
 restService.use(bodyParser.json())
 
 restService.post('/hook', function(req, res) {
-
-  retrieveUser(req, (error, user) => {
+    retrieveUser(req.body.originalRequest, (error, user) => {
     if(error) {
       res.status(500).json({error: 'Unable to retrieve user'})
     }
     const day = req.body.result.parameters.date
-    const speech =  retrieveLogFor(user, day, (error, speech) => {
+    retrieveLogFor(user, day, (error, speech) => {
       return res.json({
         speech: speech,
         displayText: speech,
@@ -32,22 +31,22 @@ restService.listen(config.webhookPort, function(){
   console.log(`server listening on 127.0.0.1:${config.webhookPort}`)
 })
 
-function retrieveUser(req, cb){
+function retrieveUser(slackRequest, cb){
 
-  if(!req.body.originalRequest) {
+  if(!slackRequest) {
     const APIAI_CONSOLE_FAKE_USER = '@ftassi'
     cb(false, APIAI_CONSOLE_FAKE_USER)
     return
   }
 
-  const source = req.body.originalRequest.source
+  const source = slackRequest.source
 
   if(!source.startsWith('slack')) {
     cb(`Invalid source: expected slack got ${source}`)
     return
   }
 
-  slack.retrieveUsername(req.body.originalRequest.data.user, (error, user) => {
+  slack.retrieveUsername(slackRequest.data.user, (error, user) => {
     if(error) {
       cb('Unable to retrieve user')
       return

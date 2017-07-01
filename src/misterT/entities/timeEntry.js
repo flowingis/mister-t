b693829f15ce5ebc15b77871ae62c56cfca640de
Francesco Tassi <tassi.francesco@gmail.com>
2017-07-01 09:19:37 +0200

    Parse timeEntry apiai entity

--- src/misterT/entities/timeEntry.js
+++ src/misterT/entities/timeEntry.js
@@ -1,1 +1,32 @@
 'use strict';
+const moment = require('moment')
+
+module.exports = function(timeEntry) {
+  return {
+    projectName: timeEntry.projectName,
+    issue: timeEntry.activity,
+    hours: hours(timeEntry.duration),
+    date: moment(timeEntry.date)
+  }
+}
+
+function hours(duration) {
+  const toHours = {
+    giorno: 8,
+    ora: 1,
+  }
+
+  if(!toHours[duration.unit]) {
+    throw new InvalidDurationUnitError(`${duration.unit} is not a valid unit of time, giorno or ora allowed`)
+  }
+
+  return duration.amount * toHours[duration.unit]
+}
+
+function InvalidDurationUnitError(message) {
+  this.name = 'InvalidDurationUnitError';
+  this.message = message || 'Default Message';
+  this.stack = (new Error()).stack;
+}
+InvalidDurationUnitError.prototype = Object.create(Error.prototype);
+InvalidDurationUnitError.prototype.constructor = InvalidDurationUnitError;
--- src/misterT/entities/timeEntry.spec.js
+++ src/misterT/entities/timeEntry.spec.js
@@ -1,1 +1,65 @@
 'use strict';
+
+require('tap').mochaGlobals();
+const should = require('should')
+const assert = require('assert')
+const timeEntry = require('./timeEntry')
+const moment = require('moment')
+
+const aTimeEntry = {
+  "date": "2017-06-30",
+  "duration": {
+    "amount": 1,
+    "unit": "giorno"
+  },
+  "activity.original": "attività di sviluppo,",
+  "duration.object": {},
+  "activity.object": {},
+  "activity": "attività di sviluppo",
+  "projectName.original": "wally",
+  "date.original": "oggi",
+  "projectName": "Wally",
+  "duration.original": "1 giorno",
+  "date.object": {},
+  "projectName.object": {}
+}
+describe('time entry', () => {
+  it('should parse project name', () => {
+    const { projectName } = timeEntry(aTimeEntry)
+    projectName.should.be.equal('Wally')
+  })
+
+  it('should parse activity', () => {
+    const { issue } = timeEntry(aTimeEntry)
+    issue.should.be.equal('attività di sviluppo')
+  })
+
+  it('should parse date', () => {
+    const {date} = timeEntry(aTimeEntry)
+    date.should.be.deepEqual(moment('2017-06-30'))
+  })
+
+  describe('duration parsing', () => {
+    it('should parse duration and convert it to hour', () => {
+      const { hours } = timeEntry(aTimeEntry)
+      hours.should.be.equal(8)
+    })
+
+    it('should parse hours', () => {
+      const { hours } = timeEntry({ duration: { amount: 4, unit: 'ora' } })
+      hours.should.be.equal(4)
+    })
+
+    it('should throw for invalid unit', () => {
+      assert.throws(
+        () => timeEntry({ duration: { amount: 4, unit: 'mese' } }),
+        err => {
+          if (err.name === 'InvalidDurationUnitError') {
+            return true
+          }
+        }
+      )
+    })
+  })
+
+})

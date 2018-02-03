a3955bee1e36debf900f184eada709f630d6778f
Francesco Tassi <tassi.francesco@gmail.com>
2018-02-03 17:06:34 +0100

    Add ideatos module

---
+++ src/ideatos.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const _ = require('lodash')
+
+const ideatos = {
+  '6': '@kea',
+  '158': '@nicol-e',
+  '160': '@pietro',
+  '151': '@dennais',
+  '173': '@marco',
+  '130': '@dymissy',
+  '120': '@ricfrank',
+  '127': '@moreno',
+  '163': '@m4s0',
+  '129': '@alemazz',
+  '169': '@mazzcris',
+  '3': '@orso',
+  '170': '@rado',
+  '171': '@uochi',
+  '95': '@ftassi',
+  '142': '@paolo',
+  '4': '@ciccio',
+  '124': '@vitto',
+  '176': '@edelprino',
+  '174': '@francesco-strazzullo',
+  '182': '@nandoluc',
+  '180': '@ramonavesprini',
+  '177': '@filippogianessi'
+};
+
+module.exports.bySlackName = (slackName) => {
+  for (const redmineId in ideatos) {
+    if (ideatos.hasOwnProperty(redmineId)) {
+        if(ideatos[redmineId] === slackName) {
+          return {redmineId: redmineId, slackId: slackName};
+        }
+    }
+  }
+};
+
+module.exports.allSlackIds = (function allSlackIds(ideatos) {
+  return _.map(ideatos, (slackId) => {
+    return slackId
+  })
+}(ideatos))
---
+++ src/ideatos.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+require('should')
+require('tap').mochaGlobals()
+const ideatos = require('./ideatos')
+
+describe('bySlackName', () => {
+  it('should retrieve an ideatos by his slack name', () => {
+    ideatos.bySlackName('@ftassi').should.be.deepEqual({redmineId: '95', slackId: '@ftassi'})
+  })
+})
+
+describe('allSlackIds', () => {
+  it('should return a list of ideatos on slack', () => {
+    (ideatos.allSlackIds).should.containEql('@ftassi')
+  })
+})

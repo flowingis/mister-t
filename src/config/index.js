74fa10d546799cc0d2933fa4357b4f3d53a9d802
Francesco Tassi <tassi.francesco@gmail.com>
2018-02-03 17:35:48 +0100

    Add dialogflow client

--- src/config/index.js
+++ src/config/index.js
@@ -6,8 +6,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 assert(process.env.SLACK_API_TOKEN, 'SLACK_API_TOKEN is missing')
+assert(process.env.DIALOG_FLOW_CLIENT_ACCESS_TOKEN, 'DIALOG_FLOW_CLIENT_ACCESS_TOKEN is missing')
 
 module.exports = {
   debug: process.env.DEBUG,
-  slackApiToken: process.env.SLACK_API_TOKEN
+  slackApiToken: process.env.SLACK_API_TOKEN,
+  dialogFlowClientAccessToken: process.env.DIALOG_FLOW_CLIENT_ACCESS_TOKEN
 }

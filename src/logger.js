b0bebbe9e8f4149e680e744970f8769c9103d0f6
Francesco Tassi <tassi.francesco@gmail.com>
2018-04-20 00:21:25 +0200

    Silence logging output in testing environment

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,11 @@
 module.exports = function () {
+  if (process.env.NODE_ENV === 'test') {
+    return {
+      debug: () => {},
+      error: () => {}
+    }
+  }
+
   return {
     debug: console.info,
     error: console.error
